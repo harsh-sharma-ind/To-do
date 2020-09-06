fe9c4b6559981c1069dcd3b2d6d3e1fb2799baab
Harsh Sharma <harsh2000hs.hs@gmail.com>
2020-09-06 23:16:27 +0530

    Updates

---
+++ precache-manifest.d7ebcf53e67b30aabe5b6968f1a64cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c0fceff717b9f75191fb708909b2ee",
+    "url": "/To-do/index.html"
+  },
+  {
+    "revision": "4057fe5d69ffc842d596",
+    "url": "/To-do/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/To-do/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4057fe5d69ffc842d596",
+    "url": "/To-do/static/js/main.59e7c77a.chunk.js"
+  },
+  {
+    "revision": "064deecfd133d1a4ca6e",
+    "url": "/To-do/static/js/runtime-main.9c6b9ea6.js"
+  }
+]);
